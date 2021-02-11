cd1f231c56bc06c686c0ad875a74c92ac5911676
Karl Guzman <guzmankarl0410@gmail.com>
2021-02-10 17:53:43 -0800

    Products page and Product component initial setup

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import Grid from "@material-ui/core";
 
+const products = [
+  { id: 1, name: "Shoes", description: "Running shoes", price: "$10" },
+  { id: 2, name: "Laptop", description: "Gaming laptop", price: "$2000" },
+];
+
 const Products = () => {
   <main>
-    <Grid container justify="center" spacing={4}></Grid>
+    <Grid container justify="center" spacing={4}>
+      {/* mock products */}
+      {products.map((product) => (
+        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+          <Product product={product} />
+        </Grid>
+      ))}
+    </Grid>
   </main>;
 };
 
