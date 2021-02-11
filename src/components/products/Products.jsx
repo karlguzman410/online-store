2d03af908c749783737c27beb3806641e44f3331
Karl Guzman <guzmankarl0410@gmail.com>
2021-02-10 18:25:23 -0800

    navbar styling added

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
+import useStyles from "./styles";
 
 const products = [
   {
@@ -22,8 +23,11 @@ const products = [
 ];
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {/* mock products */}
         {products.map((product) => (
